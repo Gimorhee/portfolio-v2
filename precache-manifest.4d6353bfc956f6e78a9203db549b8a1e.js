aff07e3419c63c194e54581cf742fbbbe39b62c3
Danny Rhee <dongyunrhee@gmail.com>
2020-03-24 20:44:54 -0700

    Updates

---
+++ precache-manifest.4d6353bfc956f6e78a9203db549b8a1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eec8f955b2f1c691021a82167d158d19",
+    "url": "/portfolio-v2/index.html"
+  },
+  {
+    "revision": "b6eae732d92f38d8154e",
+    "url": "/portfolio-v2/static/css/main.55380735.chunk.css"
+  },
+  {
+    "revision": "dc3f8ff30a25aa8f0947",
+    "url": "/portfolio-v2/static/js/2.92b605cb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio-v2/static/js/2.92b605cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6eae732d92f38d8154e",
+    "url": "/portfolio-v2/static/js/main.44449c68.chunk.js"
+  },
+  {
+    "revision": "0b7fd6723511e5e12bfa",
+    "url": "/portfolio-v2/static/js/runtime-main.2b33b786.js"
+  }
+]);
