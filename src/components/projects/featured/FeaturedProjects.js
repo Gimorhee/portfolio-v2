1d37768ee62e5a66c9f1daefec50ac94ced120c0
Danny Rhee <dongyunrhee@gmail.com>
2020-05-01 06:00:33 -0700

    Added animations to all components

--- src/components/projects/featured/FeaturedProjects.js
+++ src/components/projects/featured/FeaturedProjects.js
@@ -1,25 +1,51 @@
 import React from "react";
-
 import { BoosterProject } from "./BoosterProject";
 import { WatchMeProject } from "./WatchMeProject";
 import OnikonProjects from "./onikon/OnikonProjects";
 import PlateauProject from "./PlateauProject";
+import { useSpring, animated } from "react-spring";
+import TrackVisibility from "react-on-screen";
 
 import "../../../css/projects/projects.css";
 
 function FeaturedProjects() {
-  return (
-    <div>
+  const ComponentToTrack = ({ isVisible }) => {
+    const topProps = useSpring({
+      from: { opacity: 0 },
+      to: { opacity: isVisible ? 1 : 0 },
+      delay: 500,
+      config: { duration: 1000 },
+    });
+
+    const bottomProps = useSpring({
+      from: { opacity: 0 },
+      to: { opacity: isVisible ? 1 : 0 },
+      delay: 1200,
+      config: { duration: 1000 },
+    });
+
+    return (
       <div className="FeaturedProjects">
-        <h1 className="Project-Header">FEATURED PROJECTS</h1>
-        <div className="FeaturedProjects-Container">
+        <animated.div style={topProps}>
+          <h1 className="Project-Header">FEATURED PROJECTS</h1>
+        </animated.div>
+        <animated.div
+          style={bottomProps}
+          className="FeaturedProjects-Container"
+        >
           <BoosterProject />
           <WatchMeProject />
           <OnikonProjects />
           <PlateauProject />
-        </div>
+        </animated.div>
       </div>
-    </div>
+    );
+  };
+
+  return (
+    <TrackVisibility partialVisibility once>
+      <ComponentToTrack />
+    </TrackVisibility>
   );
 }
 
